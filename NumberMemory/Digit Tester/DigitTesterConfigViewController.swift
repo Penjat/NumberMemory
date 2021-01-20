fde2817110bf6f0e3d7c83a4c74f9cfa52837d4b
Penjat <theSpencerSymington@Gmail.com>
2021-01-20 10:02:25 -0330

    Tidy.

--- NumberMemory/Digit Tester/DigitTesterConfigViewController.swift
+++ NumberMemory/Digit Tester/DigitTesterConfigViewController.swift
@@ -4,7 +4,7 @@ class DigitTesterConfigViewController: UIViewController {
 	lazy var mainStack: UIStackView = {
 		let stack = UIStackView()
 		stack.axis = .vertical
-		stack.distribution = .equalSpacing
+		stack.distribution = .fill
 		stack.spacing = 20.0
 		return stack
 	}()
@@ -12,6 +12,9 @@ class DigitTesterConfigViewController: UIViewController {
 	let digitStack: UIStackView = {
 		let stack = UIStackView()
 		stack.axis = .horizontal
+		stack.spacing = 20
+		stack.distribution = .fill
+		stack.alignment = .center
 		return stack
 	}()
 
@@ -27,14 +30,14 @@ class DigitTesterConfigViewController: UIViewController {
 		stepper.maximumValue = 4
 		stepper.minimumValue = 1
 		stepper.value = 1
-		stepper.backgroundColor = .red
+
 		stepper.addTarget(self, action: #selector(digitStepperValueChanged(_:)), for: .touchUpInside)
 		return stepper
 	}()
 
 	let numberDigitsLabel: UILabel = {
 		let label = UILabel()
-		label.text = "1"
+		label.text = "Number of Digits: 1"
 		return label
 	}()
 
@@ -45,11 +48,13 @@ class DigitTesterConfigViewController: UIViewController {
 		mainStack.translatesAutoresizingMaskIntoConstraints = false
 		view.addSubview(mainStack)
 		mainStack.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
-		mainStack.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-		mainStack.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-		mainStack.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+		mainStack.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor).isActive = true
+		mainStack.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor).isActive = true
+		mainStack.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor).isActive = true
+
 
 		mainStack.addArrangedSubview(digitStack)
+
 		digitStack.addArrangedSubview(digitStepper)
 		digitStack.addArrangedSubview(numberDigitsLabel)
 		mainStack.addArrangedSubview(startButton)
@@ -62,6 +67,6 @@ class DigitTesterConfigViewController: UIViewController {
 	}
 
 	@objc func digitStepperValueChanged(_ sender: UIStepper) {
-		numberDigitsLabel.text = "\(Int(sender.value))"
+		numberDigitsLabel.text = "Number of Digits: \(Int(sender.value))"
 	}
 }
