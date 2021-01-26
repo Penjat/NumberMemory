f7b7610421aa38492189b39df8e63c02d4b77522
Penjat <theSpencerSymington@Gmail.com>
2021-01-26 16:54:36 -0330

    Removed unnessesary rx

---
+++ NumberMemory/Digit Tester/Domain/DigitTestAnswer.swift
@@ -0,0 +1,9 @@
+//
+//  DigitTestAnswer.swift
+//  NumberMemory
+//
+//  Created by Spencer Symington on 2021-01-25.
+//  Copyright © 2021 Spencer Symington. All rights reserved.
+//
+
+import Foundation
