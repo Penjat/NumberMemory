3dd5823ff0723cbdec8a139d4005985088ce6969
Penjat <theSpencerSymington@Gmail.com>
2021-01-31 15:38:49 -0330

    Keypad press animation.

--- NumberMemory/Helpers/UIColor+Colors.swift
+++ NumberMemory/Helpers/UIColor+Colors.swift
@@ -2,7 +2,8 @@ import UIKit
 
 extension UIColor {
 	class CustomStyle {
-		static let keypadKey: UIColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+		static let keypadKey = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+		static let keypadKeyPressed = #colorLiteral(red: 1, green: 0, blue: 0.8896911144, alpha: 1)
 		static let digitTesterBackground = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
 		static let digitTesterFlash = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
 
