06981462fa784f0d80bed93ba58c54ceb4eb843c
Penjat <theSpencerSymington@Gmail.com>
2021-01-31 16:05:17 -0330

    Added custom keyboard.

--- NumberMemory/Basic Number Transformer/BasicTransformerViewController.swift
+++ NumberMemory/Basic Number Transformer/BasicTransformerViewController.swift
@@ -7,48 +7,14 @@ class BasicTransformerViewController: UIViewController {
 	var viewModel: BasicTransformerViewModel
 	private let inputText: PublishSubject<BasicTransformerViewIntent> = .init()
 
-	init(viewModel: BasicTransformerViewModel) {
-		self.viewModel = viewModel
-		super.init(nibName: nil, bundle: nil)
-		viewModel.viewState.subscribe(onNext: { viewState in
-			self.outputText.text = viewState.phraseText
-		}).disposed(by: disposeBag)
-	}
-
-	required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-		view.backgroundColor = .systemPink
-		setUpViews()
-		numberInputField.becomeFirstResponder()
-		numberInputField.rx.controlEvent(.editingChanged).withLatestFrom(numberInputField.rx.text.orEmpty).subscribe(onNext: { text in
-			self.viewModel.processIntent(intent: .inputText(text))
-		}).disposed(by: disposeBag)
-    }
-
-	private func setUpViews(){
-		view.addSubview(mainStack)
-		mainStack.translatesAutoresizingMaskIntoConstraints = false
-		mainStack.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-		mainStack.bottomAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-		mainStack.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-		mainStack.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-		mainStack.addArrangedSubview(numberInputField)
-		mainStack.addArrangedSubview(outputText)
-
-
-	}
-
-	lazy var outputText: UILabel = {
-		let label = UILabel()
-		label.text = "____"
-		label.numberOfLines = 0
-		return label
+	//MARK: Views
+	lazy var mainStack: UIStackView = {
+		let stack = UIStackView()
+		stack.axis = .vertical
+		stack.alignment = .fill
+		stack.distribution = .fillEqually
+		return stack
 	}()
-
 
 	lazy var numberInputField: UITextField = {
 		let textField = UITextField()
@@ -58,11 +24,51 @@ class BasicTransformerViewController: UIViewController {
 		return textField
 	}()
 
-	lazy var mainStack: UIStackView = {
-		let stackView = UIStackView()
-		stackView.axis = .vertical
-		stackView.distribution = .fillEqually
-		stackView.alignment = .center
-		return	stackView
+	lazy var outputText: UILabel = {
+		let label = UILabel()
+		label.text = "____"
+		label.numberOfLines = 0
+		return label
 	}()
+
+	let keypad = DigitKeypad(keyNames: ["0","1","2","3","4","5","6","7","8","9"])
+
+	init(viewModel: BasicTransformerViewModel) {
+		self.viewModel = viewModel
+		super.init(nibName: nil, bundle: nil)
+		viewModel.viewState.subscribe(onNext: { viewState in
+			self.outputText.text = viewState.phraseText
+		}).disposed(by: disposeBag)
+	}
+
+	//MARK: Init
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		view.backgroundColor = .systemPink
+		setUpViews()
+		keypad.output.subscribe(onNext: { output in
+			switch output {
+			case .pressedKey(number: let keyNumber):
+				self.viewModel.processIntent(intent: .inputText("\(keyNumber)"))
+			}
+			}).disposed(by: disposeBag)
+    }
+
+	private func setUpViews(){
+		view.addSubview(mainStack)
+		mainStack.translatesAutoresizingMaskIntoConstraints = false
+		mainStack.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+		mainStack.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+		mainStack.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+		mainStack.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+
+		mainStack.addArrangedSubview(numberInputField)
+		mainStack.addArrangedSubview(outputText)
+		mainStack.addArrangedSubview(keypad)
+		keypad.widthAnchor.constraint(equalTo: mainStack.widthAnchor).isActive = true
+	}
 }
